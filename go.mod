c072699e388d30528f9603a4eb5558662c26e723
polyhistor2050 <chrisspaul2050@gmail.com>
2024-05-25 19:03:37 +0300

    Create a server instance

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/TheKnighttron/rssagg
 
 go 1.22.3
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
@@ -16,6 +18,19 @@ func main() {
     portString := os.Getenv("PORT")
     if portString == " " {
         log.Fatal("PORT is not found in the environment variable")
+    }
+
+    router := chi.NewRouter()
+
+    serve := &http.Server{
+        Handler: router,
+        Addr: ":" + portString,
+    }
+
+    log.Printf("Server is running of port: %v ", portString)
+    err := serve.ListenAndServe()
+    if err != nil {
+        log.Fatal("An error occured: ", err)
     }
 
 
