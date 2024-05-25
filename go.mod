bca22c4ed6dad4e39c016ada1d205834f3e6345c
polyhistor2050 <chrisspaul2050@gmail.com>
2024-05-25 18:13:21 +0300

    Load all the environment variables from .env to the main package

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/TheKnighttron/rssagg
+
+go 1.22.3
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+    godotenv.Load(".env")
+
+    portString := os.Getenv("PORT")
+    if portString == " " {
+        log.Fatal("PORT is not found in the environment variable")
+    }
+
+
+    fmt.Println("PORT: ", portString)
+}
