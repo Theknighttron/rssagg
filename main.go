b134b50216009e36ab11ce9d8248a47ea46a5bc2
polyhistor2050 <chrisspaul2050@gmail.com>
2024-05-26 13:48:47 +0300

    Create struct to make make connection to the database

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"github.com/TheKnighttron/rssagg/internal/database"
 	"github.com/go-chi/chi"
-    "github.com/go-chi/cors"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+    DB *database.Queries
+}
 
 func main() {
 	fmt.Println("Hello, World!")
@@ -20,6 +28,24 @@ func main() {
     portString := os.Getenv("PORT")
     if portString == " " {
         log.Fatal("PORT is not found in the environment variable")
+    }
+
+    dbUrl := os.Getenv("DB_URL")
+    if dbUrl == " " {
+        log.Fatal("DB_URL is not found in the environment variable")
+    }
+
+
+    // Connect to the database
+    conn, err := sql.Open("postgres", dbUrl)
+    if err != nil {
+        log.Fatal("Can't connect to the database")
+    }
+
+    // Convert to db query
+    queries := database.New(conn)
+    apiCfg := apiConfig{
+        DB: queries,
     }
 
     router := chi.NewRouter()
@@ -37,6 +63,7 @@ func main() {
     v1Router := chi.NewRouter()
     v1Router.Get("/healthz", handlerReadiness)
     v1Router.Get("/err", handlerError)
+    v1Router.Post("/users", apiCfg.handlerCreateUser)
 
     // Mount all v1Router to /v1 path
     router.Mount("/v1", v1Router)
@@ -47,7 +74,7 @@ func main() {
     }
 
     log.Printf("Server is running of port: %v ", portString)
-    err := serve.ListenAndServe()
+    err = serve.ListenAndServe()
     if err != nil {
         log.Fatal("An error occured: ", err)
     }
