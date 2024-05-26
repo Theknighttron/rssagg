73c3c1bd1580c6ce8bc07d3cf80619706641bf77
polyhistor2050 <chrisspaul2050@gmail.com>
2024-05-26 13:49:20 +0300

    Handler to create new user

---
+++ handler_user.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/TheKnighttron/rssagg/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+    type paramaters struct {
+        Name string `json:"name"`
+    }
+
+    params := paramaters{}
+
+    decoder := json.NewDecoder(r.Body)
+    err := decoder.Decode(&params)
+    if err != nil {
+        respondWithError(w, 400, fmt.Sprintf("Error parsinf JSON %v", err))
+        return
+    }
+
+    user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+        ID: uuid.New(),
+        CreatedAt: time.Now().UTC(),
+        UpdatedAt: time.Now().UTC(),
+        Name: params.Name,
+    })
+    if err != nil {
+        respondWithError(w, 400, fmt.Sprintf("Couldn't create user %v", err))
+        return
+    }
+
+    respondWithJSON(w, 200, user)
+}
